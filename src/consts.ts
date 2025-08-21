20a04452af365c10955fa58607822d04fa8d78c9
chxuo <hi@chxuo.com>
2025-08-21 21:20:12 +0800

    Initial website, use Astro Micro theme

--- src/consts.ts
+++ src/consts.ts
@@ -1,40 +1,42 @@
 import type { Metadata, Site, Socials } from "@types";
 
 export const SITE: Site = {
-  TITLE: "Astro Micro",
-  DESCRIPTION: "Astro Micro is an accessible and lightweight blog.",
-  EMAIL: "trevortylerlee@gmail.com",
+  TITLE: "CHXUO",
+  DESCRIPTION: "业余全栈开发者，希望能在开心快乐每一天的同时，做点有用或者有趣儿的东西",
+  EMAIL: "hi@chxuo.com",
   NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
-  TITLE: "Home",
-  DESCRIPTION: "Astro Micro is an accessible theme for Astro.",
+  TITLE: "主页",
+  DESCRIPTION: "业余全栈开发者，希望能在开心快乐每一天的同时，做点有用或者有趣儿的东西",
 };
 
 export const BLOG: Metadata = {
-  TITLE: "Blog",
-  DESCRIPTION: "A collection of articles on topics I am passionate about.",
+  TITLE: "博客",
+  DESCRIPTION: "我感兴趣的东西都在这里了",
 };
 
 export const PROJECTS: Metadata = {
-  TITLE: "Projects",
-  DESCRIPTION:
-    "A collection of my projects with links to repositories and live demos.",
+  TITLE: "项目",
+  DESCRIPTION: "做的一些小玩意儿",
 };
 
 export const SOCIALS: Socials = [
   {
-    NAME: "X (formerly Twitter)",
-    HREF: "https://twitter.com/boogerbuttcheek",
-  },
-  {
     NAME: "GitHub",
-    HREF: "https://github.com/trevortylerlee",
+    HREF: "https://github.com/chxuo",
+    ICON: "mingcute:github-line",
   },
   {
     NAME: "Website",
-    HREF: "https://trevortylerlee.com",
+    HREF: "https://chxuo.com",
+    ICON: "mingcute:web-line",
+  },
+  {
+    NAME: "Email",
+    HREF: "mailto:hi@chxuo.com",
+    ICON: "mingcute:mail-line",
   },
 ];
