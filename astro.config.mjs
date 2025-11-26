7a9cc2ac986fa5554e465773dbb449265070541a
chxuo <hi@chxuo.com>
2025-11-26 23:16:51 +0800

    feat: add base layout and header component

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 import mdx from "@astrojs/mdx";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://chxuo.com",
@@ -13,5 +15,5 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 
-  integrations: [mdx()],
+  integrations: [mdx(), icon()],
 });
---
+++ src/config/site.ts
@@ -0,0 +1,25 @@
+import type { NavItem } from "@/types/nav";
+
+export const NAV: NavItem[] = [
+  {
+    title: "首页",
+    href: "/",
+    icon: "tabler:home",
+  },
+  {
+    title: "文章",
+    href: "/index",
+    icon: "tabler:news",
+  },
+  {
+    title: "友链",
+    href: "/friends",
+    icon: "tabler:link",
+  },
+  {
+    title: "关于",
+    href: "/about",
+    icon: "tabler:shirt",
+    // icon: "tabler:terminal-2",
+  },
+];
